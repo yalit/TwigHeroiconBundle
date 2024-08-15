2980f5bb4d6d49f954a5307bf97c6c3ef7349c62
Yannick <tech@yalit.be>
2024-08-15 23:04:02 +0200

    build : allow for bundle configuration

--- src/TwigHeroiconBundle.php
+++ src/TwigHeroiconBundle.php
@@ -2,14 +2,45 @@
 
 namespace Yalit\TwigHeroiconBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Yalit\TwigHeroiconBundle\Twig\TwigHeroiconExtension;
 
 class TwigHeroiconBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition
+            ->rootNode()
+            ->children()
+            ->scalarNode('heroicon_getter')
+            ->defaultValue('yalit.heroicon.getter.webpack')
+            ->end()
+            ->scalarNode('with_webpack')
+            ->defaultTrue()
+            ->end()
+            ->scalarNode('default_display_type')
+            ->defaultValue('outline')
+            ->end()
+            ->scalarNode('default_size')
+            ->defaultValue('24')
+            ->end()
+            ->end();
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->import('./Resources/config/services.yaml');
+        $container->import('../config/services.yaml');
+
+        $heroiconGetter = $builder->getDefinition($config['heroicon_getter']);
+        $container
+            ->services()
+            ->get(TwigHeroiconExtension::class)
+            ->arg(0, $heroiconGetter)
+            ->arg(1, $config['with_webpack'])
+            ->arg(2, $config['default_display_type'])
+            ->arg(3, $config['default_size']);
     }
 }
